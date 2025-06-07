eed3863c48d60c3a4bbbac2bf7b4d535dc01aa11
Chrystian Melo <chrysmelao@gmail.com>
2025-06-07 15:15:01 -0300

    Refactoring

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import os
 import urllib.request
 import filtrar_csv
 
-def baixar_arquivo_se_necessario(url, nome_arquivo):
-    if os.path.exists(nome_arquivo):
-        print(f"[✓] O arquivo '{nome_arquivo}' já existe. Nenhum download necessário.")
+def downloadFile(url, filename):
+    """
+    Baixa um arquivo da URL fornecida, caso ele ainda não exista localmente.
+
+    Parâmetros:
+        url (str): URL do arquivo a ser baixado.
+        filename (str): Nome do arquivo local onde o conteúdo será salvo.
+    """
+    if os.path.exists(filename):
+        print(f"[✓] O arquivo '{filename}' já existe. Nenhum download necessário.")
         return
 
-    print(f"[↓] Baixando '{nome_arquivo}'...")
+    print(f"[↓] Baixando '{filename}'...")
 
     req = urllib.request.Request(
         url,
@@ -16,16 +23,16 @@ def baixar_arquivo_se_necessario(url, nome_arquivo):
 
     try:
         with urllib.request.urlopen(req) as response:
-            with open(nome_arquivo, 'wb') as f:
+            with open(filename, 'wb') as f:
                 f.write(response.read())
-        print(f"[✔] Download concluído e salvo como '{nome_arquivo}'")
+        print(f"[✔] Download concluído e salvo como '{filename}'")
     except Exception as e:
         print(f"[✗] Erro ao baixar: {e}")
 
 if __name__ == '__main__':
     url = "https://ckan.pbh.gov.br/dataset/ec3efaac-0ca6-4846-9e32-0ffff2d76dbb/resource/a35a0ed3-c933-4919-b23f-b925c37b64b8/download/20241001_atividade_economica.csv"
-    nome_arquivo = "20241001_atividade_economica.csv"
+    filename = "20241001_atividade_economica.csv"
     
-    baixar_arquivo_se_necessario(url, nome_arquivo)
+    downloadFile(url, filename)
 
     filtrar_csv.main()
